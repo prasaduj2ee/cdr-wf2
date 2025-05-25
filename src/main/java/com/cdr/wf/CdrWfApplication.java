aadb51e9e84b71f13729326ac78a7b07f05e6ff6
prasaduj2ee <89792710+prasaduj2ee@users.noreply.github.com>
2025-05-26 00:09:12 +0530

    Add files via upload

---
+++ src/main/java/com/cdr/wf/CdrWfApplication.java
@@ -0,0 +1,13 @@
+package com.cdr.wf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrWfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdrWfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdr/wf/CdrWfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdr.wf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrWfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
